7716c7f8f658b40e3a4ef5fb7ace6c7a1a3a705c
Bedolaga3 <maks.chernyshov.98@list.ru>
2022-05-19 16:27:27 +0300

    bebra

---
+++ src/main/java/com/itproger/blog/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.itproger.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home( Model model) {
+        model.addAttribute("title", "Главная Страница");
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about( Model model) {
+        model.addAttribute("title", "Главная Страница");
+        return "home";
+    }
+}
