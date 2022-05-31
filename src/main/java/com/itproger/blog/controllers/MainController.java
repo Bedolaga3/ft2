c1e714180e8c403c75c95fe72bdcfb86c45f1e03
MDayZZ <maxdayZZOff@gmail.com>
2022-05-31 14:31:29 +0300

    Backend: Created imageApi Frontend: 1. deleted Main page 2. Created button Generate and Gallery

--- src/main/java/com/itproger/blog/controllers/MainController.java
+++ src/main/java/com/itproger/blog/controllers/MainController.java
@@ -3,9 +3,13 @@ package com.itproger.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+
 
     @GetMapping("/")
     public String home( Model model) {
@@ -15,6 +19,15 @@ public class MainController {
     @GetMapping("/about")
     public String about( Model model) {
         model.addAttribute("title", "Главная Страница");
-        return "home";
+
+        return "about";
+    }
+
+    @PostMapping("/generate")
+    public String generate(@RequestParam("CountOfPics") String CountOfPics) {
+
+
+
+        return "Ok";
     }
 }
---
+++ src/main/java/com/itproger/blog/controllers/imageController.java
@@ -0,0 +1,94 @@
+package com.itproger.blog.controllers;
+
+
+import com.itproger.blog.exceptions.NotFoundException;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.activation.FileTypeMap;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+@RestController
+@RequestMapping("images")
+public class imageController {
+    private String pathToResultFolder = "C:/Users/ASUS/Desktop/springboot/ft2/src/main/resources/results"; //Введите путь до папки result
+
+    private int counter = new File(pathToResultFolder).listFiles().length;
+
+    private List<Map<String, String>> images = new ArrayList<Map<String, String>>() {{
+
+        for(int i = counter-1; i>-1; i--){
+            String StringI = Integer.toString(i);
+            add(new HashMap<String, String>() {{ put("id", StringI); put("url", "/images/showme/" + StringI);}});
+        }
+        //add(new HashMap<String, String>() {{ put("id", "1"); put("url", "https://i.pinimg.com/600x315/04/fc/74/04fc74e8816759bb4bb8e9a61bd67981.jpg");}});
+        //add(new HashMap<String, String>() {{ put("id", "2"); put("url", "/images/showme");}});
+        //add(new HashMap<String, String>() {{ put("id", "3"); put("url", "https://i.pinimg.com/600x315/04/fc/74/04fc74e8816759bb4bb8e9a61bd67981.jpg");}});
+
+    }};
+    @GetMapping()
+    public List<Map<String, String>> list() {
+        return images;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getImage(id);
+    }
+
+    @GetMapping(value = "/showme/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<Resource> image(@PathVariable String id) throws IOException {
+        final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
+                pathToResultFolder + "/"+ id + ".png"
+        )));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentLength(inputStream.contentLength())
+                .body(inputStream);
+
+    }
+
+    private Map<String, String> getImage(String id) {
+        return images.stream()
+                .filter(image -> image.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> image) {
+        image.put("id", String.valueOf(counter++));
+        Collections.reverse(images);
+        images.add(image);
+        Collections.reverse(images);
+
+        return image;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> image) {
+        Map<String, String> imageFromDb = getImage(id);
+
+        imageFromDb.putAll(image);
+        imageFromDb.put("id", id);
+
+        return imageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> image = getImage(id);
+
+        images.remove(image);
+    }
+
+
+}
